a8b0f65ac5193b5318413af7ae1180c8004e93c4
Kasinan Rordthab <sameyu.ping@gmail.com>
2024-03-25 09:08:22 +0700

    Update Kotlin version and Fixed error onClickListener

--- app/src/main/java/com/example/kotlin_101/FirstFragment.kt
+++ app/src/main/java/com/example/kotlin_101/FirstFragment.kt
@@ -22,7 +22,7 @@ class FirstFragment : Fragment() {
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
@@ -32,7 +32,7 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.buttonFirst.setOnClickListener {
+        binding.randomButton.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }
     }
